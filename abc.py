d995aa4e671c1da123c9157cad8702ea6eca6782
Sunny Kathuria <41035660+sunny-kathuria@users.noreply.github.com>
2021-05-28 22:38:20 +0530

    All done!

--- abc.py
+++ abc.py
@@ -1,21 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
-
 final=''
 temp=''
-for i in range(1,101):
+for i in range(54,91):
    a=str(i)
-   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-   respo =requests.get("https://passcomptia.com/comptia-security/comptia-security-question-f-"+a,headers=headers)  # Change the page
-   abc=respo.text 
+   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'} #To bypass request-blocking from WAF
+   respo =requests.get("https://passcomptia.com/comptia-security/comptia-security-question-j-"+a,headers=headers)  # Change the page
    soup= BeautifulSoup(abc, 'html.parser')
-   #print(abc)
-   content=soup.find('div',{"class": "entry-content"})
-   #ques=content.find('p')
-   #ans=content.find('div',{"class": "hidden-div"})
-   #temp="Ques."+a+" "+str(ques)+"\n"+str(ans)
+   content=soup.find('div',{"class": "entry-content"}) 
    print("Ques."+a+str(content))
 
 
